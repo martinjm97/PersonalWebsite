4d39825db4f62cf6a30b2ad8cf96ddacdd1debb8
Zack Holbrook <zackh@mit.edu>
2019-06-29 23:01:57 -0700

    Initial Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import Button from 'antd/es/button';
+import Menu from 'antd/es/menu';
+import Icon from 'antd/es/icon';
 import './App.css';
 
+const { SubMenu } = Menu;
+
 class App extends Component {
+  
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = (e: any) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="name">
+          Name
+        </Menu.Item>
+        <Menu.Item key="experience">
+          Experience
+        </Menu.Item>
+        <Menu.Item key="projects">
+          Projects
+        </Menu.Item>
+      </Menu>
+      
       </div>
     );
   }
