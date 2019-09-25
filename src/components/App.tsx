eab8a6f9bff8db4abb379ae0786a48485067966f
jmmichel <jmmichel@mit.edu>
2019-09-25 00:34:48 -0400

    added a new page and broke up tests and components into new folders

---
+++ src/components/App.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// import Button from 'antd/es/button';
+import Menu from 'antd/es/menu';
+// import Icon from 'antd/es/icon';
+import './App.scss';
+import NamePage from './NamePage'
+
+// const { SubMenu } = Menu;
+
+class App extends Component {
+
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = (e: any) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="name">
+          Name
+        </Menu.Item>
+        <Menu.Item key="experience">
+          Experience
+        </Menu.Item>
+        <Menu.Item key="projects">
+          Projects
+        </Menu.Item>
+      </Menu>
+      <NamePage />
+      </div>
+    );
+  }
+}
+
+export default App;
