96f75802c20ed1430d7d45a8dc8c218d3f971358
Zack Holbrook <zackh@mit.edu>
2019-09-25 00:15:34 -0400

    dev quality of life

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
-import Button from 'antd/es/button';
-import Menu from 'antd/es/menu';
-import Icon from 'antd/es/icon';
-import './App.css';
-
-const { SubMenu } = Menu;
+import Menu from "antd/es/menu";
+import React, { Component } from "react";
+import "./App.scss";
 
 class App extends Component {
-  
-  state = {
-    current: 'mail',
+
+  public state = {
+    current: "mail",
   };
 
-  handleClick = (e: any) => {
-    console.log('click ', e);
+  public handleClick = (e: any) => {
+    console.log("click ", e);
     this.setState({
       current: e.key,
     });
-  };
+  }
 
-  render() {
+  public render() {
     return (
       <div className="App">
         <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-        <Menu.Item key="name">
-          Name
-        </Menu.Item>
-        <Menu.Item key="experience">
-          Experience
-        </Menu.Item>
-        <Menu.Item key="projects">
-          Projects
-        </Menu.Item>
-      </Menu>
-      
+          <Menu.Item key="name">
+            Name
+          </Menu.Item>
+          <Menu.Item key="experience">
+            Experience
+          </Menu.Item>
+          <Menu.Item key="projects">
+            Projects
+          </Menu.Item>
+        </Menu>
+
       </div>
     );
   }
