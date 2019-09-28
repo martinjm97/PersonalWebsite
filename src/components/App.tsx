e19d5ff15cba26b6a6853a431e71446abb9baacd
Zack Holbrook <zackh@mit.edu>
2019-09-28 10:47:37 -0400

    main layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
-// import Button from 'antd/es/button';
+import { Layout } from "antd";
 import Menu from "antd/es/menu";
 import React, { Component } from "react";
-// import Icon from 'antd/es/icon';
-import "./App.scss";
 import NamePage from "./NamePage";
 
-// const { SubMenu } = Menu;
+import "./App.scss";
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
 
@@ -22,20 +22,26 @@ class App extends Component {
 
   public render() {
     return (
-      <div className="App">
-        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-        <Menu.Item key="name">
-          Name
-        </Menu.Item>
-        <Menu.Item key="experience">
-          Experience
-        </Menu.Item>
-        <Menu.Item key="projects">
-          Projects
-        </Menu.Item>
-      </Menu>
-      <NamePage />
-      </div>
+      <Layout>
+      <Header>
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]}
+            mode="horizontal" className="Menu" style={{ lineHeight: "64px" }}>
+          <Menu.Item key="name">
+            Name
+          </Menu.Item>
+          <Menu.Item key="experience">
+            Experience
+          </Menu.Item>
+          <Menu.Item key="projects">
+            Projects
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{minHeight: "calc(100vh - 134px)" }}>
+        <NamePage />
+      </Content>
+      <Footer style={{ textAlign: "center" }}>Say some things in the footer</Footer>
+    </Layout>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import "./index.scss";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./index.scss";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
