9d20b1f1fd5813056a88c1234f17126ee89e3c99
jmmichel <jmmichel@mit.edu>
2019-09-28 01:56:43 -0400

    routing and pages are set up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 // import Button from 'antd/es/button';
 import Menu from "antd/es/menu";
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
 // import Icon from 'antd/es/icon';
 import "./App.scss";
+import ExperiencePage from "./ExperiencePage";
 import NamePage from "./NamePage";
+import ProjectsPage from "./ProjectsPage";
 
 // const { SubMenu } = Menu;
 
@@ -23,18 +27,23 @@ class App extends Component {
   public render() {
     return (
       <div className="App">
-        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-        <Menu.Item key="name">
-          Name
-        </Menu.Item>
-        <Menu.Item key="experience">
-          Experience
-        </Menu.Item>
-        <Menu.Item key="projects">
-          Projects
-        </Menu.Item>
-      </Menu>
-      <NamePage />
+        <Router>
+          <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+            <Menu.Item key="name">
+              <Link to="/">Name</Link>
+            </Menu.Item>
+            <Menu.Item key="experience">
+              <Link to="/experience">Experience</Link>
+            </Menu.Item>
+            <Menu.Item key="projects">
+              <Link to="/projects">Projects</Link>
+            </Menu.Item>
+          </Menu>
+
+          <Route exact path="/" component={NamePage} />
+          <Route path="/experience" component={ExperiencePage} />
+          <Route path="/projects" component={ProjectsPage} />
+        </Router>
       </div>
     );
   }
