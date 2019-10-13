6a19469bd2b77d69cd231407b80ef07df55fb9e7
Zack Holbrook <zackh@mit.edu>
2019-10-12 23:37:31 -0400

    pages have stuff

--- src/components/ExperiencePage.tsx
+++ src/components/ExperiencePage.tsx
@@ -6,7 +6,26 @@ class NamePage extends Component {
   public render() {
     return (
       <div className="NamePage">
-        Experience World
+        <h2 className="projectTitle"> Experience 1</h2>
+        <img className="projectPlaceholderImg"></img>
+        <p className="projectText"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis sint dignissimos reprehenderit ratione delectus ad ea nisi quidem enim tempora, temporibus consequuntur aspernatur voluptatibus molestiae minus cum praesentium, deleniti voluptatem.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam nihil non inventore eos expedita eius vero illum velit eveniet, ipsum itaque accusamus laborum perferendis fuga porro voluptatibus fugiat commodi.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vitae quo fuga voluptatibus autem.
+        </p>
+
+        <h2 className="projectTitle"> Experience 2</h2>
+        <img className="projectPlaceholderImg"></img>
+        <p className="projectText"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis sint dignissimos reprehenderit ratione delectus ad ea nisi quidem enim tempora, temporibus consequuntur aspernatur voluptatibus molestiae minus cum praesentium, deleniti voluptatem.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam nihil non inventore eos expedita eius vero illum velit eveniet, ipsum itaque accusamus laborum perferendis fuga porro voluptatibus fugiat commodi.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vitae quo fuga voluptatibus autem.
+        </p>
+
+        <h2 className="projectTitle"> Experience 3</h2>
+        <img className="projectPlaceholderImg"></img>
+        <p className="projectText"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis sint dignissimos reprehenderit ratione delectus ad ea nisi quidem enim tempora, temporibus consequuntur aspernatur voluptatibus molestiae minus cum praesentium, deleniti voluptatem.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam nihil non inventore eos expedita eius vero illum velit eveniet, ipsum itaque accusamus laborum perferendis fuga porro voluptatibus fugiat commodi.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vitae quo fuga voluptatibus autem.
+        </p>
       </div>
     );
   }
