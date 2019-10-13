3f15d9eeef6f3e98e531d5cb04fd0f886483a192
Zack Holbrook <zackh@mit.edu>
2019-10-12 22:19:08 -0400

    name change

--- src/components/NamePage.tsx
+++ src/components/NamePage.tsx
@@ -6,7 +6,7 @@ class NamePage extends Component {
   public render() {
     return (
       <div className="NamePage" style={{maxWidth: 800, margin: "0 auto", textAlign:"left"}}>
-        <h1 style={{textAlign:"left", marginTop: "10px", fontSize:"32px"}}> Zack Holbrook</h1>
+        <h1 style={{textAlign:"left", marginTop: "10px", fontSize:"32px"}}> John Doe-Smith</h1>
         <img style={{width: 150, height: 150, backgroundColor: "#444", float:"right", marginLeft: "10px"}}></img>
         <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis sint dignissimos reprehenderit ratione delectus ad ea nisi quidem enim tempora, temporibus consequuntur aspernatur voluptatibus molestiae minus cum praesentium, deleniti voluptatem.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam quisquam nihil non inventore eos expedita eius vero illum velit eveniet, ipsum itaque accusamus laborum perferendis fuga porro voluptatibus fugiat commodi.
